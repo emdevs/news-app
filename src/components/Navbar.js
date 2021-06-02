cd7ec0601a019478adeb59e1aad2bbd78d5500e0
Emmy <emerentia.arjani@gmail.com>
2021-06-02 16:26:36 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = (props) => {
+    
+    const { tabNames } = props;
+
+    return (
+        <div id="navbar">
+            <p>This is the navbar!</p>
+        </div>
+    );
+};
+
+export default Navbar;
